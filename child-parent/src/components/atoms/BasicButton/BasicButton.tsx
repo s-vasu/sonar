8d468daef3af000afbbd19fc139ad7c6be4d1a22
s-vasu <vasu.suryavamsham@zemosolabs.com>
2024-09-27 00:14:28 +0530

    button test edited

--- child-parent/src/components/atoms/BasicButton/BasicButton.tsx
+++ child-parent/src/components/atoms/BasicButton/BasicButton.tsx
@@ -5,26 +5,27 @@ import theme  from '../../../themes/theme';
 
 
 
+const StyledButton = styled(Button)`
+  &.reviewbtn {
+    background-color: ${theme.palette.third.main};
+    width: 100%;
+    height: 59px;
+    border: 0 solid grey;
+    border-radius: 12px;
+    margin-top:5% !important;
+  }
+  &.resetbtn {
+    color: white;
+    background-color: ${theme.palette.fourth.main};
+    border: 0 solid grey;
+    border-radius: 12px;
+    text-transform: capitalize;
+    padding: 6px 12px;
+  }
+`;
+
 export default function BasicButton({ variant, text, size, className, onClick }:{variant?:"text"|"outlined"|"contained",className?:string, text:string,size?:"small" | "medium" | "large",onClick?:(event: React.MouseEvent<HTMLButtonElement>) => void}) {
 
-    const StyledButton = styled(Button)`
-      &.reviewbtn {
-        background-color: ${theme.palette.third.main};
-        width: 100%;
-        height: 59px;
-        border: 0 solid grey;
-        border-radius: 12px;
-        margin-top:5% !important;
-      }
-      &.resetbtn {
-        color: white;
-        background-color: ${theme.palette.fourth.main};
-        border: 0 solid grey;
-        border-radius: 12px;
-        text-transform: capitalize;
-        padding: 6px 12px;
-      }
-    `;
   return (
     <StyledButton className={className} size={size} variant={variant} onClick={onClick}>
       {text}
